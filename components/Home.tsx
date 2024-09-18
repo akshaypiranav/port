1aed8908ff3e61f217a195b9edb48a5c35472e8b
AKSHAY PIRANAV B <130298522+akshaypiranav@users.noreply.github.com>
2024-09-18 18:18:50 +0530

    Update Home.tsx

--- components/Home.tsx
+++ components/Home.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import Link from "next/link"
-
+import { useState, useEffect } from "react";
 
 const handleDownload = () => {
   const resumeUrl = '/resume.pdf';
@@ -14,6 +14,40 @@ const handleDownload = () => {
 };
 
 const Home = () => {
+  const [text, setText] = useState(" "); 
+  const [isTyping, setIsTyping] = useState(true); 
+  const [currentTitleIndex, setCurrentTitleIndex] = useState(0); 
+  const titles = [
+    "FULL-STACK DEVELOPER",
+    "PYTHON DEVELOPER",
+    "SOFTWARE ENGINEER - INTERN",
+    "NODEJS DEVELOPER"
+  ];
+
+  useEffect(() => {
+    const currentTitle = titles[currentTitleIndex];
+    if (isTyping) {
+      if (text.length < currentTitle.length) {
+        setTimeout(() => {
+          setText(currentTitle.slice(0, text.length + 1));
+        }, 100);
+      } else {
+        setTimeout(() => {
+          setIsTyping(false);
+        }, 1000); 
+      }
+    } else {
+      if (text.length > 0) {
+        setTimeout(() => {
+          setText(currentTitle.slice(0, text.length - 1));
+        }, 50); 
+      } else {
+        setIsTyping(true);
+        setCurrentTitleIndex((prevIndex) => (prevIndex + 1) % titles.length); 
+      }
+    }
+  }, [text, isTyping, currentTitleIndex]);
+
   return (
     <div className="container " style={{marginTop : "70px"}} data-aos="zoom-in">
       <div className="row justify-content-center">
@@ -29,32 +63,34 @@ const Home = () => {
         </div>
         <div className="col-md-6 col-sm-12 mt-4 text-center">
           <h1 className="display-1 text fw-bold mt-2">I'M AKSHAY PIRANAV</h1>
-          <h3 className="fw-bold typing">FULL-STACK DEVELOPER</h3>
+          <h3 className="fw-bold typing">{text}</h3>
           <h6 className="fw-bold my-4">💎 NodeJS / Python 💎</h6>
-          <div className="row justify-content-center ">
+          <div className="row justify-content-center">
             
             <div className="col-1">
-            <Link href="https://www.linkedin.com/in/akshaypiranavb/" target="_blank" >
-            <i className="fa fa-linkedin" aria-hidden="true"></i>
-            </Link>
+              <Link href="https://www.linkedin.com/in/akshaypiranavb/" target="_blank" >
+                <i className="fa fa-linkedin" aria-hidden="true"></i>
+              </Link>
             </div>
             <div className="col-1">
-            <Link href="https://www.instagram.com/akshaypiranav/" target="_blank" >
-            <i className="fa fa-instagram" aria-hidden="true"></i>            </Link>
+              <Link href="https://www.instagram.com/akshaypiranav/" target="_blank" >
+                <i className="fa fa-instagram" aria-hidden="true"></i>
+              </Link>
             </div>
             <div className="col-1">
-            <Link href="mailto:akshaypiranavb@hotmail.com" target="_blank" >
-            <i className="fa fa-envelope-o" aria-hidden="true"></i>
-            </Link>
+              <Link href="mailto:akshaypiranavb@hotmail.com" target="_blank" >
+                <i className="fa fa-envelope-o" aria-hidden="true"></i>
+              </Link>
             </div>
             <div className="col-1">
-            <Link href="https://github.com/akshaypiranav" target="_blank" >
-            <i className="fa fa-github" aria-hidden="true"></i>
-            </Link>
+              <Link href="https://github.com/akshaypiranav" target="_blank" >
+                <i className="fa fa-github" aria-hidden="true"></i>
+              </Link>
             </div>
             <div className="text-center mt-4">
-            <button className="btn btn-danger  fw-bold" onClick={handleDownload}><i className="fa fa-download me-1" aria-hidden="true"></i>
-            Resume</button>
+              <button className="btn btn-danger fw-bold" onClick={handleDownload}>
+                <i className="fa fa-download me-1" aria-hidden="true"></i> Resume
+              </button>
             </div>
           </div>
         </div>
@@ -63,4 +99,4 @@ const Home = () => {
   )
 }
 
-export default Home
+export default Home;
