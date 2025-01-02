b531ac91f7932fbef1747293acfb5e19566d4af5
AKSHAY PIRANAV B <130298522+akshaypiranav@users.noreply.github.com>
2025-01-02 11:19:13 +0530

    Update Contact.tsx

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -3,7 +3,7 @@ const Contact = () => {
   return (
     <div className="container my-5" id="contact" data-aos="fade-up">
         <h1 className="text-center text fw-bold">☎Get In Touch</h1>
-        <h5 className="text-center">Vasantham Nagar, Sattur, Virudhunagar,</h5>
+        <h5 className="text-center">Sattur,Madurai</h5>
         <h6 className="text-center">TamilNadu.</h6>
         <Link  href="https://www.linkedin.com/in/akshaypiranavb/" target="_blank"><p className="text-center">☎️9025370736</p></Link>
 
